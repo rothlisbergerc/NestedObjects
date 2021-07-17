6292f4268529450f4108ee89502d608fd6dff39a
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-17 10:15:18 -0700

    Added Instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an individual Instructor.
+    /// </summary>
+    class Instructor
+    {
+        /// <summary>
+        /// Legal full name. First and last. 
+        /// Example: Jane Doe.
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email address.
+        /// Example: Jane.Doe@cptc.edu.
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
